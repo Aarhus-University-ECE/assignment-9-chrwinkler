ea5f22a8669b2aa463fde6cfe6edce90e5ce91fc
chrwinkler <101516854+chrwinkler@users.noreply.github.com>
2022-11-03 13:37:11 +0100

    Search completed

--- src/search.c
+++ src/search.c
@@ -3,9 +3,11 @@
 
 bool search(int a[], int n, int x)
 {
-    // pre-condiontiom
-    // her igen får vi en erkendelse om at
+    // pre-condiontion
+    // her igen får vi en erkendelse om at den er falsk hvis den er n=0 eller n<0 derfor bruges else
+    // benytter else if og else if for både at få når a[n-1]==x og a[n-1]!=x som giver true of search.
 
+    // post-condition
     if (n > 0 && a[n - 1] == x)
     {
         return true;
