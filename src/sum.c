cef8f1708bc1c2bcdfca78c8010061ed8176f6a1
chrwinkler <101516854+chrwinkler@users.noreply.github.com>
2022-11-03 13:13:15 +0100

    sum completed

--- src/sum.c
+++ src/sum.c
@@ -2,5 +2,20 @@
 
 int sum(int a[], int n)
 {
-    return 0;
+    // pre-condition
+    // vi ved at hvis n = 0 skal den returne 0
+    // vi har heller ikke nogen condition for n<0 så der vil den også returne 0
+    // derfor benytter vi else og ikke else if
+
+    // post-condition
+    // for en værdi printede med mindre n =< 0 så returner den 0.
+    // n > 0 returner den totale sum.
+    if (n > 0)
+    {
+        return a[n - 1] + sum(a, n - 1);
+    }
+    else
+    {
+        return 0;
+    }
 }
