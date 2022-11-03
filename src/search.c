9532aaa0b175adb728429b512258103a5d68c9e7
chrwinkler <101516854+chrwinkler@users.noreply.github.com>
2022-11-03 13:33:03 +0100

    search completed

--- src/search.c
+++ src/search.c
@@ -3,5 +3,19 @@
 
 bool search(int a[], int n, int x)
 {
-    return 0;
+    // pre-condiontiom
+    // her igen får vi en erkendelse om at
+
+    if (n > 0 && a[n - 1] == x)
+    {
+        return true;
+    }
+    else if (n > 0 && a[n - 1] != x)
+    {
+        return search(a, n - 1, x);
+    }
+    else
+    {
+        return false;
+    }
 }
